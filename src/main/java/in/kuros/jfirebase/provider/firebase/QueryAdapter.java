4cfe3d5713c8a2878ed345f840dbbf35f3b614cd
Kumar Rohit <incrediblevicky@gmail.com>
2021-01-12 00:52:32 +0530

    add collection group support

--- src/main/java/in/kuros/jfirebase/provider/firebase/QueryAdapter.java
+++ src/main/java/in/kuros/jfirebase/provider/firebase/QueryAdapter.java
@@ -1,6 +1,5 @@
 package in.kuros.jfirebase.provider.firebase;
 
-import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
 import in.kuros.jfirebase.provider.firebase.query.QueryBuilder;
 import in.kuros.jfirebase.query.Query;
@@ -10,7 +9,9 @@ public class QueryAdapter {
     public static <T> com.google.cloud.firestore.Query toFirebaseQuery(final Firestore firestore, final Query<T> query) {
         final QueryBuilder<T> queryBuilder = (QueryBuilder<T>) query;
         final String path = queryBuilder.getPath();
-        final CollectionReference collectionReference = firestore.collection(path);
-        return queryBuilder.build(collectionReference);
+        if (queryBuilder.isCollectionGroup()) {
+            return queryBuilder.build(firestore.collectionGroup(path));
+        }
+        return queryBuilder.build(firestore.collection(path));
     }
 }
--- src/main/java/in/kuros/jfirebase/provider/firebase/query/QueryBuilder.java
+++ src/main/java/in/kuros/jfirebase/provider/firebase/query/QueryBuilder.java
@@ -3,53 +3,58 @@ package in.kuros.jfirebase.provider.firebase.query;
 import com.google.common.collect.Lists;
 import in.kuros.jfirebase.metadata.Attribute;
 import in.kuros.jfirebase.metadata.AttributeValue;
-import in.kuros.jfirebase.metadata.UpdateAttribute;
 import in.kuros.jfirebase.provider.firebase.AttributeValueHelper;
 import in.kuros.jfirebase.provider.firebase.EntityHelper;
 import lombok.Getter;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public final class QueryBuilder<T> extends QueryImpl<T> {
     @Getter private final String path;
     @Getter private Class<T> resultType;
+    @Getter private boolean collectionGroup;
     private final AttributeValueHelper attributeValueHelper;
 
     @SuppressWarnings("unchecked")
-    private QueryBuilder(final Class<T> type) {
-        this(getCollectionName(type), type);
-    }
 
-    private QueryBuilder(final String path, final Class<T> resultType) {
+    private QueryBuilder(final String path, final Class<T> resultType, final boolean collectionGroup) {
         this.path = path;
         this.resultType = resultType;
         this.attributeValueHelper = new AttributeValueHelper();
+        this.collectionGroup = collectionGroup;
     }
 
     public static <S> QueryBuilder<S> collection(final Class<S> type) {
-        return new QueryBuilder<>(type);
+        return new QueryBuilder<>(getCollectionName(type), type, false);
     }
 
     public static <S> QueryBuilder<S> collection(final String path, final Class<S> resultType) {
-        return new QueryBuilder<>(path, resultType);
+        return new QueryBuilder<>(path, resultType, false);
+    }
+
+    public static <S> QueryBuilder<S> collectionGroup(final Class<S> type) {
+        return new QueryBuilder<>(getCollectionName(type), type, true);
+    }
+
+    public static <S> QueryBuilder<S> collectionGroup(final String path, final Class<S> resultType) {
+        return new QueryBuilder<>(path, resultType, true);
     }
 
     public <S> QueryBuilder<S> subCollection(final Class<S> type) {
-        return new QueryBuilder<>(path + "/" + getCollectionName(type), type);
+        return new QueryBuilder<>(path + "/" + getCollectionName(type), type, collectionGroup);
     }
 
     public <S> QueryBuilder<S> subCollection(final String collection, final Class<S> resultClass) {
-        return new QueryBuilder<>(path + "/" + collection, resultClass);
+        return new QueryBuilder<>(path + "/" + collection, resultClass, collectionGroup);
     }
 
     public QueryBuilder<T> withId(final String id) {
-        return new QueryBuilder<>(path + "/" + id, resultType);
+        return new QueryBuilder<>(path + "/" + id, resultType, collectionGroup);
     }
 
     public QueryBuilder<T> withPath(final String pathValue) {
-        return new QueryBuilder<>(path + pathValue, resultType);
+        return new QueryBuilder<>(path + pathValue, resultType, collectionGroup);
     }
 
     @SuppressWarnings("unchecked")
@@ -65,11 +70,11 @@ public final class QueryBuilder<T> extends QueryImpl<T> {
         final T entity = attributeValueHelper.createEntity(attributeValues);
         final String documentPath = EntityHelper.INSTANCE.getDocumentPath(entity);
 
-        return new QueryBuilder<>(documentPath, attributeValues.get(0).getAttribute().getDeclaringType());
+        return new QueryBuilder<>(documentPath, attributeValues.get(0).getAttribute().getDeclaringType(), false);
     }
 
     public <S> QueryBuilder<S> resultType(final Class<S> resultClass) {
-        return new QueryBuilder<>(path, resultClass);
+        return new QueryBuilder<>(path, resultClass, collectionGroup);
     }
 
     private static String getCollectionName(final Class<?> type) {
